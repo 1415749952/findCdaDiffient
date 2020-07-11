f082dd7481cfd07890a1577d5fe140051598c14a
thefei <liu1415749952@foxmail.com>
2020-07-11 10:08:37 +0800

    急诊留观病例将过敏史标志默认为false改为NA

--- src/main/java/cda/RedCDA.java
+++ src/main/java/cda/RedCDA.java
@@ -23,14 +23,15 @@ public class RedCDA
     public static void main(String[] args) throws IOException
     {
         File directory = new File("");//参数为空
-        String courseFile = directory.getCanonicalPath() + "\\src\\main\\resources\\cdaTemp";//标准的路径 ;
+        String  separator= File.separator;
+        String courseFile = directory.getCanonicalPath() +separator+"src"+separator+"main"+separator+"resources"+separator+"cdaTemp";//标准的路径 ;
         File cadTempFile = new File(courseFile);
         //所有的cda文档名称
         String[] cdaFileNames = cadTempFile.list();
 
         for (String cdaFileName : cdaFileNames)
         {
-            File cad = new File(courseFile + "\\" + cdaFileName);
+            File cad = new File(courseFile + separator + cdaFileName);
             redCDAFile(cad);
         }
         //redCDAFile(new File(courseFile+"\\"+"01.病历概要.xml"));
@@ -57,6 +58,7 @@ public class RedCDA
                     System.out.println(s);
                 }
             }*/
+            //没有配置从字典取值的节点
             /*if (s.contains("code=\"${") && s.contains("!'NA'}"))
             {
                 Pattern p = Pattern.compile("\\{(.*?)\\}");//正则表达式，取{和=}之间的字符串
@@ -67,26 +69,28 @@ public class RedCDA
                     String group = m.group(0);
                     strings.add(group);
                 }
-                if (strings.size() >= 3)
+                if (strings.size() != 2)
                 {
                     System.out.println(s);
-                }*/
-            /*if (s.contains("value=\""))
+                }
+            }*/
+            //查看没有配字典的包含value字段的节点
+           /* if (s.contains("value=\""))
             {
                 if (!s.contains("${"))
                 {
                     System.out.println(s);
                 }
-                //System.out.println(s);
             }*/
+            //查看extension，中配置了值的节点
            /* if (s.contains("extension=\""))
             {
                 if (!s.contains("NA") && !s.contains("typeId") && !s.contains("${"))
                 {
                     System.out.println(s);
                 }
-                //System.out.println(s);
             }*/
+            //查看没有配字典的 <name></name>节点
             /*if (s.contains("<name>"))
             {
                 if (!s.contains("${"))
@@ -95,32 +99,30 @@ public class RedCDA
                 }
                 //System.out.println(s);
             }*/
-
+            //查看没有配字典的 <value></value>节点
             /*if (s.contains("<value") && s.contains(" displayName=\""))
             {
                 if (!s.contains("${"))
                 {
                     System.out.println(s);
                 }
-                //
             }*/
-
+            //查看没有配字典的<code></code>节点
             /*if (s.contains("code") && s.contains(" displayName=\""))
             {
                 if (!s.contains("${"))
                 {
                     System.out.println(s);
                 }
-                //
             }*/
-            if (s.contains("true") || s.contains("flase"))
+            //查看cda模版是否有配置常量true和false情况
+            /*if (s.contains("true") || s.contains("false"))
             {
                 if (!s.contains("component") && !s.contains("entry"))
                 {
                     System.out.println(s);
                 }
-
-            }
+            }*/
         }
     }
 }
